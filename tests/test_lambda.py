e841e19a6f6ba469ed97ccddd1b52d2f99b4a674
Saku Arho <sakurho@gmail.com>
2025-02-20 15:43:50 +0200

    Add a test for the lambda function.

---
+++ tests/test_lambda.py
@@ -0,0 +1,9 @@
+from lambda_code.hello_lambda import lambda_handler
+
+
+def test_lambda_handler():
+    event = {}  # Dummy event
+    context = None  # Dummy context
+    response = lambda_handler(event, context)
+    assert response["statusCode"] == 200
+    assert "Hello" in response["body"]
