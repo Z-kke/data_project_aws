56ff837f09a42b33b48b9a0d67ce7712245a3b3d
Saku Arho <sakurho@gmail.com>
2025-02-20 15:44:19 +0200

    Add terraform's configurations for the added lambda function.

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create an IAM role for Lambda execution
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+# Define the Lambda function resource
+resource "aws_lambda_function" "hello_lambda" {
+  function_name = "hello_lambda"
+  handler       = "hello_lambda.lambda_handler"
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = "${path.module}/../lambda-code/hello_lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda-code/hello_lambda.zip")
+}
+
---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "lambda_function_name" {
+  value = aws_lambda_function.hello_lambda.function_name
+}
+
